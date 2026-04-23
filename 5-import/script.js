8dd50901b3277176fe767d360da03909e7f4d8af
MCDLCN <martin.desbois@le-campus-numerique.fr>
2026-04-23 11:45:55 +0200

    Promises done

--- 5-import/script.js
+++ 5-import/script.js
@@ -1,8 +1,17 @@
 //5-1 Import
 
-import {hello} from './functions.js';
+import {hello, showError,default as nyanCat} from './functions.js';
+import * as math from './math.js';
 
 hello();
+showError("aaaaaaaaaaaaaa");
+//nyanCat();
 
 // 5-2 Export
 const [a, b] = [7, 9];
+
+console.log(math.sum(a,b))
+console.log(math.sub(a,b))
+console.log(math.multiply(a,b))
+console.log(math.divide(a,b))
+console.log(math.pow(a,b))
--- 6-promises/script.js
+++ 6-promises/script.js
@@ -21,6 +21,97 @@ function secondWithError() {
   return sleep(100).then(() => {throw new Error("catch me if you can");})
 }
 
+/*
 first();
 second();
 third();
+*/
+
+
+//first().then(second).then(third);
+
+//first().then(secondWithError).then(third);
+
+
+//first().then(secondWithError).catch(error => console.log(error)).then(third);
+
+
+
+//Second section 
+
+function fetchHumans() {
+  return fetch('https://swapi.py4e.com/api/species/1/')
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+//Shows human json
+/*fetchHumans().then(data => {
+  console.log(data);
+});
+*/
+
+//Shows first human
+/*fetchHumans().then(data => {
+  fetch(data.people[0])
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err =>{
+    console.error(err)
+  })
+});
+*/
+
+//Display the name of all characters of a species
+function fetchSpeciesName(id){
+  fetch(`https://swapi.py4e.com/api/species/${id}/`)
+  .then(response => response.json())
+  .then(species => {
+    return Promise.all(
+      species.people.map(url =>
+        fetch(url).then(res => res.json())
+      )
+    );
+  })
+  .then(peopleData => {
+      peopleData.forEach(person => {
+        console.log(person.name);
+      });
+    })
+    .catch(err => console.error(err));
+}
+
+//fetchSpeciesName(1);
+
+
+//Third section
+
+const button = document.createElement("button");
+button.textContent = "OK";
+
+document.body.appendChild(button);
+
+const button2 = document.createElement("button");
+
+button2.textContent = "Cancel";
+
+document.body.appendChild(button2);
+
+function buttoning(){
+  return new Promise((resolve, reject) => {
+    button.onclick = () => resolve("OK clicked");
+    button2.onclick = () => reject("Cancel clicked");
+    }
+  );
+}
+
+buttoning()
+.then(result => console.log("Success:", result))
+.catch(error => console.log("Failure:",error))
